0e508a170dac4c7b3e8eb7728565d4ef27c039aa
schimaere <jad.enders@gmail.com>
2018-11-27 16:15:52 +0100

    feat: streaming-subscribe is working

--- nats-streaming/nats-streaming-subscribe.js
+++ nats-streaming/nats-streaming-subscribe.js
@@ -1,19 +1,34 @@
 "use-strict";
-var stan = require('node-nats-streaming').connect('test-cluster', 'test-subscribe', {url:"nats://10.0.2.15:4223"});
 
 module.exports = function(RED) {
     function NatsStreamingSubscribeNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        this.server = RED.nodes.getNode(config.server);
 
-        // Subscriber can specify how many existing messages to get.
-        var opts = stan.subscriptionOptions().setStartWithLastReceived();
-        var subscription = stan.subscribe('foo', opts);
-        subscription.on('message', function (msg) {
-            let msgToSend;
-            msgToSend = { payload: ('Received a message [' + msg.getSequence() + '] ' + msg.getData()) };
-            node.send(msgToSend)
+        let stan = require('node-nats-streaming')
+            .connect(this.server.cluster, config.clientID, { url:'nats://' + this.server.server + ':' + this.server.port });
+
+        var subscription
+        stan.on('connect', function () {
+            // Subscriber can specify how many existing messages to get.
+            var opts = stan.subscriptionOptions().setStartWithLastReceived();        
+            subscription = stan.subscribe(config.channel, opts);
+            subscription.on('message', function (msg) {
+                let msgToSend;
+                msgToSend = { payload: ('Received a message [' + msg.getSequence() + '] ' + msg.getData()) };
+                node.send(msgToSend)
+            });
+        });
+
+        // on node close the nats stream subscription is and connection is also closed
+        node.on('close', function() {
+            subscription.unsubscribe();
+            subscription.on('unsubscribed', function() {
+                stan.close();
+            });
         });
     }
+
     RED.nodes.registerType("nats-streaming-subscribe",NatsStreamingSubscribeNode);
 }
