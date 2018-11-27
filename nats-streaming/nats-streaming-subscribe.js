ecc33485d2f92c0f5b95dc54c8369eb3e09c7f8f
schimaere <jad.enders@gmail.com>
2018-11-27 11:39:32 +0100

    feat: added streaming subscribe node

---
+++ nats-streaming/nats-streaming-subscribe.js
@@ -0,0 +1,20 @@
+"use-strict";
+var stan = require('node-nats-streaming').connect('test-cluster', 'test-subscribe', {url:"nats://10.0.2.15:4223"});
+
+module.exports = function(RED) {
+    function NatsStreamingSubscribeNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        // Subscriber can specify how many existing messages to get.
+        var opts = stan.subscriptionOptions().setStartWithLastReceived();
+        var subscription = stan.subscribe('foo', opts);
+        subscription.on('message', function (msg) {
+            let msgToSend;
+            // console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData());
+            msgToSend = { payload: ('Received a message [' + msg.getSequence() + '] ' + msg.getData()) };
+            node.send(msgToSend)
+        });
+    }
+    RED.nodes.registerType("nats-streaming-subscribe",NatsStreamingSubscribeNode);
+}
