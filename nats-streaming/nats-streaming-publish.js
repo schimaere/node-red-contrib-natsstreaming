1f430df6dd4e721eae4ef5ee38f4a215714c4f50
schimaere <jad.enders@gmail.com>
2018-11-29 15:36:50 +0100

    feat: added error handling and status to connection

--- nats-streaming/nats-streaming-publish.js
+++ nats-streaming/nats-streaming-publish.js
@@ -2,29 +2,54 @@
 
 const checkMSG = require('../checkMSG');
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function NatsStreamingPublishNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         let node = this;
+        node.status({
+            fill: "red",
+            shape: "ring",
+            text: "disconnected"
+        });
         this.server = RED.nodes.getNode(config.server);
 
         // connect to nats streaming server
         let stan = require('node-nats-streaming')
-            .connect(this.server.cluster, config.clientID, { url:'nats://' + this.server.server + ':' + this.server.port });
+            .connect(this.server.cluster, config.clientID, {
+                url: 'nats://' + this.server.server + ':' + this.server.port
+            });
+        
+        // sets status to connected
+        stan.on('connect', function() {
+            node.status({
+                fill: "green",
+                shape: "dot",
+                text: "connected"
+            });
+        })
+
+        
+
+        stan.on('error', function (err) {
+            node.log(err);
+            node.error('Could not connect to server', err);
+        });
+
+
 
         // on input send message
-        node.on('input', function(msg) {
+        node.on('input', function (msg) {
             let message;
             let channel;
-            
+
             //checks if msg has configurations and sets them
-            if(checkMSG(msg.payload)) {
+            if (checkMSG(msg.payload)) {
                 message = msg.payload;
             } else {
                 message = config.message;
             }
 
-            if(checkMSG(msg.channel)) {
+            if (checkMSG(msg.channel)) {
                 channel = msg.channel;
             } else {
                 channel = config.channel;
@@ -32,21 +57,26 @@ module.exports = function(RED) {
 
             node.log(channel);
             node.log(message);
-            stan.publish(channel, message, function(err, guid){
-                if(err) {
-                node.log('publish failed: ' + err);
-                node.err('problem while publishing message', msg)
+            stan.publish(channel, message, function (err, guid) {
+                if (err) {
+                    node.log('publish failed: ' + err);
+                    node.err('problem while publishing message', msg)
                 } else {
-                node.log('published message with guid: ' + guid);
+                    node.log('published message with guid: ' + guid);
                 }
             });
         });
 
         // on node close the nats stream connection is also closed
-        node.on('close', function() {
+        node.on('close', function () {
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: "disconnected"
+            });
             stan.close();
         });
 
     }
-    RED.nodes.registerType("nats-streaming-publish",NatsStreamingPublishNode);
+    RED.nodes.registerType("nats-streaming-publish", NatsStreamingPublishNode);
 }
--- nats-streaming/nats-streaming-subscribe.js
+++ nats-streaming/nats-streaming-subscribe.js
@@ -1,21 +1,39 @@
 "use-strict";
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function NatsStreamingSubscribeNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
+        this.status({
+            fill: "red",
+            shape: "ring",
+            text: "disconnected"
+        });
         this.server = RED.nodes.getNode(config.server);
 
         let stan = require('node-nats-streaming')
-            .connect(this.server.cluster, config.clientID, { url:'nats://' + this.server.server + ':' + this.server.port });
+            .connect(this.server.cluster, config.clientID, {
+                url: 'nats://' + this.server.server + ':' + this.server.port
+            });
 
+        stan.on('error', function (err) {
+            node.log(err);
+            node.error('Could not connect to server', err);
+        });
+        
         let subscription
         stan.on('connect', function () {
+            node.status({
+                fill: "green",
+                shape: "dot",
+                text: "connected"
+            });
+
             // Subscriber can specify how many existing messages to get.
             let opts = stan.subscriptionOptions();
 
             // set the starting point in the stream, default is last received
-            switch(config.start) {
+            switch (config.start) {
                 case 'last_received':
                     opts.setStartWithLastReceived();
                     break;
@@ -28,13 +46,12 @@ module.exports = function(RED) {
                 case 'at_date':
                     let timeParts;
                     let startTime
-                    try{
+                    try {
                         timeParts = config.start_option.split('-');
-                        startTime = new Date(timeParts[0], timeParts[1] -1, timeParts[2], 0,0,0,0);
+                        startTime = new Date(timeParts[0], timeParts[1] - 1, timeParts[2], 0, 0, 0, 0);
+                    } catch (err) {
+                        node.error('wrong format in start option. Must be YYYY-MM-DD', msg);
                     }
-                    catch(err) {
-                        node.error('wrong format in start option. Must be YYYY-MM-DD', msg);                        
-                    }                    
                     opts.setStartTime(startTime)
                     break;
                 case 'at_time':
@@ -46,33 +63,42 @@ module.exports = function(RED) {
             }
 
             // if durable is true sets it
-            if(config.durable) {
+            if (config.durable) {
+                node.log("set durable name");
                 opts.setDurableName(config.durable_name);
             }
 
             // if queue group is true sets it
-            if(config.queue_group) {
+            if (config.queue_group) {
                 subscription = stan.subscribe(config.channel, config.queue_group_name, opts);
             } else {
                 subscription = stan.subscribe(config.channel, opts);
             }
-            
+
             subscription.on('message', function (msg) {
                 let msgToSend;
-                msgToSend = { payload: msg.getData(), sequence: msg.getSequence()};
+                msgToSend = {
+                    payload: msg.getData(),
+                    sequence: msg.getSequence()
+                };
                 node.send(msgToSend)
             });
         });
 
         // on node close the nats stream subscription is and connection is also closed
-        node.on('close', function(done) {
+        node.on('close', function (done) {
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: "disconnected"
+            });
             subscription.unsubscribe();
-            subscription.on('unsubscribed', function() {
+            subscription.on('unsubscribed', function () {
                 stan.close();
                 done();
             });
         });
     }
 
-    RED.nodes.registerType("nats-streaming-subscribe",NatsStreamingSubscribeNode);
+    RED.nodes.registerType("nats-streaming-subscribe", NatsStreamingSubscribeNode);
 }
